78ce619243f718c462a831715d3a9f2449f9b8fd
MishaZorin <165201586+MishaZorin@users.noreply.github.com>
2024-09-23 19:06:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-let cases = document.getElementById('cases')
+
 let services = document.getElementById('services')
 let agent = document.getElementById('agent')
 let click = document.getElementById('click')
 let click_modal = document.getElementById('click_modal')
 let cases_blog = document.getElementById('cases_blog')
+let services_modal_show = false
+let agenstvo_show = false
 let services_modal = document.getElementById('services_modal')
 let agenstvo = document.getElementById('agenstvo')
-cases.onclick=function(){
-    cases_blog.style.display = 'block'
-}
 services.onclick=function(){
-    services_modal.style.display = 'block'
+    services_modal_show = !services_modal_show
+    if (services_modal_show ){
+        services_modal.style.display = 'block'
+
+    }
+    else{
+        services_modal.style.display = 'none'
+    }
+    
 }
 agent.onclick=function(){
-    agenstvo.style.display = 'block'
+    agenstvo_show = !agenstvo_show
+    if(agenstvo_show){
+        agenstvo.style.display = 'block'
+
+    }
+    else{
+        agenstvo.style.display = 'none'
+    }
+    
+
 }
 click.onclick=function(){
     click_modal.style.display = 'block'
